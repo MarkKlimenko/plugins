d2fcc538a2dbf22d7a3a79a38112be05d8d80abd
MarkKlimenko <22773@bk.ru>
2017-09-16 13:55:56 +1000

    Change project version regex

--- set-minor-version/src/main/groovy/com/markklim/plugins/smv/SetMinorVersion.groovy
+++ set-minor-version/src/main/groovy/com/markklim/plugins/smv/SetMinorVersion.groovy
@@ -17,7 +17,7 @@ class SetMinorVersion implements Plugin<Project> {
                 ["git", "checkout", ciBranchName].execute().text
 
                 def sourceFile = new File('gradle.properties')
-                def versionField = (sourceFile.text =~ /^projectVersion.+\b/)[0]
+                def versionField = (sourceFile.text =~ /projectVersion.+\b/)[0]
                 def rawVersionField = versionField.split('\\.')
 
                 if (minorVersion.size() != 0) {
@@ -26,7 +26,7 @@ class SetMinorVersion implements Plugin<Project> {
                     } else {
                         versionField = "${rawVersionField[0]}.${rawVersionField[1]}.${minorVersion}"
                     }
-                    sourceFile.write(sourceFile.text.replaceAll(/^projectVersion.+\b/, versionField))
+                    sourceFile.write(sourceFile.text.replaceAll(/projectVersion.+\b/, versionField))
                 }
 
                 ["git", "add", "gradle.properties"].execute().text
